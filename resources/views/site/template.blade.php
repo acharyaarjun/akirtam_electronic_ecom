13918746eb695b0d07a0d50e4282619398c4e09c
Arjun Acharya <legendarjun333@gmail.com>
2023-05-30 09:02:21 +0545

    Navbar section starts

--- resources/views/site/template.blade.php
+++ resources/views/site/template.blade.php
@@ -54,13 +54,62 @@
     </section>
     {{-- top-header section ends here --}}
 
+    {{-- navbar section starts here --}}
+    <section id="top-header-navbar">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="{{ asset('site/image/logo.png') }}" alt="LOGO" title="AKIRTAM ELECTRONIC ECOM"
+                        class="img-fluid">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto text-center">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Our Services</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Category
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Lorem, ipsum dolor.Laptop</a></li>
+                                <li><a class="dropdown-item" href="#">Mobile</a></li>
+                                <li><a class="dropdown-item" href="#">Watch</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+    {{-- navbar section ends here --}}
+
 
 
     {{-- jquery link gareko --}}
     <script src="{{ asset('site/jquery/jquery.js') }}"></script>
 
+    {{-- propoer js ko javascript link gareko --}}
+    <script src="{{ asset('site/bootstrap/proper.js') }}"></script>
+
     {{-- bootstrap ko javascript lai link gareko --}}
     <script src="{{ asset('site/bootstrap/bootstrap.js') }}"></script>
+
 
     {{-- fontawesome ko js link gareko --}}
     <script src="{{ asset('site/fontawesome/all.js') }}"></script>
