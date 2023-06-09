4e72164edb917aecc58d8710a5acbd09560a6953
Arjun Acharya <legendarjun333@gmail.com>
2023-06-09 08:20:44 +0545

    service section done

--- resources/views/site/template.blade.php
+++ resources/views/site/template.blade.php
@@ -319,6 +319,43 @@
     </section>
     {{-- prdoducts section end here --}}
 
+    {{-- service section starts here --}}
+    <section id="service">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="service-box">
+                        <i class="fa-solid fa-truck-fast icon"></i>
+                        <h2>Free Shipping</h2>
+                        <p>Order over $100</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="service-box">
+                        <i class="fa-solid fa-shield icon"></i>
+                        <h2>Secure Payment</h2>
+                        <p>100% Secure Payment</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="service-box">
+                        <i class="fa-solid fa-award icon"></i>
+                        <h2>Best Price</h2>
+                        <p>Guaranted Low Cost</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="service-box">
+                        <i class="fa-solid fa-right-left icon"></i>
+                        <h2>Easy Return</h2>
+                        <p>Within 30 Days Returns</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- service section ends here --}}
+
 
 
     {{-- jquery link gareko --}}
