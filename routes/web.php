d383b2d3571ff2877722bb8fba3f569da041e278
Arjun Acharya <legendarjun333@gmail.com>
2023-06-14 08:56:51 +0545

    default authenication generate on our webiste

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\SiteController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,7 @@ Route::get('/', [SiteController::class, 'getHome'])->name('getHome');
 Route::get('/about', [SiteController::class, 'getAbout'])->name('getAbout');
 Route::get('/service', [SiteController::class, 'getService'])->name('getService');
 Route::get('/contact', [SiteController::class, 'getContact'])->name('getContact');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
